63fe509262db115c7d7c6b39333f0bae3cee2931
hatuank97lhp <hatuank97lhp@gmail.com>
2023-08-11 23:06:05 +0700

    fix

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -65,7 +65,6 @@ exports.queryDepositInforByUserAddress = async (req, res) => {
             receiver = BigInt(receiver).toString(10);
         }
         let response = [];
-        let x = [];
         if (receiver == "") {
             response = await DepositInfor.find({ sender: sender });
         } else {
@@ -75,16 +74,56 @@ exports.queryDepositInforByUserAddress = async (req, res) => {
                 response = await DepositInfor.find({ sender: sender, eth_receiver: receiver });
             }
         }
+
+        // set status
+        const tree = await DepositTree.findOne();
+        let ans = [];
         for (let i = 0; i < response.length; i++) {
-            x.push(response[i]);
+            const infor = response[i];
+            let inforDeposit = {
+                is_deposit: response[i].is_deposit,
+                sender: response[i].sender,
+                destination_chainid: response[i].destination_chainid,
+                eth_bridge_address: response[i].eth_bridge_address,
+                eth_receiver: response[i].eth_receiver,
+                amount: response[i].amount,
+                eth_token_address: response[i].eth_token_address,
+                cosmos_token_address: response[i].cosmos_token_address,
+                key: response[i].key,
+                value: response[i].value
+            };
+            if(infor.key >= tree.n_leafs) {
+                inforDeposit["status"] = "IN_QUEUE";
+            } else {
+                const publicInput = {
+                    eth_bridge_address: convertHexStringToAddress(bigNumberToHexString(infor.eth_bridge_address)),
+                    eth_receiver: convertHexStringToAddress(bigNumberToHexString(infor.eth_receiver)),
+                    amount: infor.amount,
+                    eth_token_address: convertHexStringToAddress(bigNumberToHexString(infor.eth_token_address)),
+                    key: infor.key
+                };
+                const isSent = await isSentProof(
+                    publicInput.eth_bridge_address,
+                    publicInput.eth_receiver,
+                    publicInput.amount,
+                    publicInput.eth_token_address,
+                    publicInput.key
+                );
+                if (isSent) {
+                    inforDeposit["status"] = "CLAIMED";
+                } else {
+                    inforDeposit["status"] = "CAN_CLAIM";
+                }
+            }
+            ans.push(inforDeposit);
         }
-        res.status(200).send(x);
+
+        res.status(200).send(ans);
     } catch (err) {
         console.log(err);
         res.status(500).send({ err: err.toString() });
     }
 }
-
 exports.cronjobUpdate = async (req, res) => {
     try {
         console.log("------------cronjob auto update------------")
